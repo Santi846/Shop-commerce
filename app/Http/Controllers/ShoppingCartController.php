69f067c5c8d602ce44565d35cdfb83d349d96719
Santiago Arocha <arochas846@gmail.com>
2023-12-25 14:04:41 -0300

    Settig things up

---
+++ app/Http/Controllers/ShoppingCartController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\shopping_cart;
+use Illuminate\Http\Request;
+
+class ShoppingCartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(shopping_cart $shopping_cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(shopping_cart $shopping_cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, shopping_cart $shopping_cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(shopping_cart $shopping_cart)
+    {
+        //
+    }
+}
